d0a190a5f5fb1aef785b847ba66a4197c93e6f46
Mehedi Hasan <mehedi.hasandeveloper26@gmail.com>
2021-06-05 22:37:18 +0600

    Removed sticky scripts and CSS

--- js/footer.js
+++ js/footer.js
@@ -29,12 +29,4 @@ $(document).ready(function () {
 
 })
 
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 1) {
-        $('.header').addClass("sticky");
-    }
-    else {
-        $('.header').removeClass("sticky");
-    }
-});
 
--- js/main.js
+++ js/main.js
@@ -3,97 +3,79 @@
  * main js
  *
  * ------------------------------------------------------------------- 
- */ 
+ */
 
-(function($) {
+(function ($) {
 
 	"use strict";
 
 	/*---------------------------------------------------- */
 	/* Preloader
-	------------------------------------------------------ */ 
-   $(window).load(function() {
+	------------------------------------------------------ */
+	$(window).load(function () {
 
-      // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
+		// will first fade out the loading animation 
+		$("#loader").fadeOut("slow", function () {
 
-        // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
+			// will fade out the whole DIV that covers the website.
+			$("#preloader").delay(300).fadeOut("slow");
 
-      });       
+		});
 
-  	})
+	})
 
-
-	/*----------------------------------------------------*/
-	/*	Sticky Navigation
-	------------------------------------------------------*/
-   $(window).on('scroll', function() {
-
-		var y = $(window).scrollTop(),
-		    topBar = $('header');
-     
-	   if (y > 1) {
-	      topBar.addClass('sticky');
-	   }
-      else {
-         topBar.removeClass('sticky');
-      }
-    
-	});
-	
 
 	/*-----------------------------------------------------*/
-  	/* Mobile Menu
-   ------------------------------------------------------ */  
-   var toggleButton = $('.menu-toggle'),
-       nav = $('.main-navigation');
+	/* Mobile Menu
+------------------------------------------------------ */
+	var toggleButton = $('.menu-toggle'),
+		nav = $('.main-navigation');
 
-   toggleButton.on('click', function(event){
+	toggleButton.on('click', function (event) {
 		event.preventDefault();
 
 		toggleButton.toggleClass('is-clicked');
 		nav.slideToggle();
 	});
 
-  	if (toggleButton.is(':visible')) nav.addClass('mobile');
+	if (toggleButton.is(':visible')) nav.addClass('mobile');
 
-  	$(window).resize(function() {
-   	if (toggleButton.is(':visible')) nav.addClass('mobile');
-    	else nav.removeClass('mobile');
-  	});
+	$(window).resize(function () {
+		if (toggleButton.is(':visible')) nav.addClass('mobile');
+		else nav.removeClass('mobile');
+	});
 
-  	$('#main-nav-wrap li a').on("click", function() {   
+	$('#main-nav-wrap li a').on("click", function () {
 
-   	if (nav.hasClass('mobile')) {   		
-   		toggleButton.toggleClass('is-clicked'); 
-   		nav.fadeOut();   		
-   	}     
-  	});
+		if (nav.hasClass('mobile')) {
+			toggleButton.toggleClass('is-clicked');
+			nav.fadeOut();
+		}
+	});
 
 
-   /*----------------------------------------------------*/
-  	/* Highlight the current section in the navigation bar
-  	------------------------------------------------------*/
+	/*----------------------------------------------------*/
+	/* Highlight the current section in the navigation bar
+	------------------------------------------------------*/
 	var sections = $("section"),
-	navigation_links = $("#main-nav-wrap li a");	
+		navigation_links = $("#main-nav-wrap li a");
 
-	sections.waypoint( {
+	sections.waypoint({
 
-       handler: function(direction) {
+		handler: function (direction) {
 
-		   var active_section;
+			var active_section;
 
 			active_section = $('section#' + this.element.id);
 
 			if (direction === "up") active_section = active_section.prev();
 
-			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');			
+			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');
 
-         navigation_links.parent().removeClass("current");
+			navigation_links.parent().removeClass("current");
 			active_link.parent().addClass("current");
 
-		}, 
+		},
 
 		offset: '25%'
 
@@ -101,55 +83,55 @@
 
 
 	/*----------------------------------------------------*/
-  	/* Flexslider
-  	/*----------------------------------------------------*/
-  	$(window).load(function() {
+	/* Flexslider
+	/*----------------------------------------------------*/
+	$(window).load(function () {
 
-	   $('#testimonial-slider').flexslider({
-	   	namespace: "flex-",
-	      controlsContainer: "",
-	      animation: 'slide',
-	      controlNav: true,
-	      directionNav: true,
-	      smoothHeight: true,
-	      slideshowSpeed: 7000,
-	      animationSpeed: 600,
-	      randomize: false,
-	      touch: true,
-	   });
+		$('#testimonial-slider').flexslider({
+			namespace: "flex-",
+			controlsContainer: "",
+			animation: 'slide',
+			controlNav: true,
+			directionNav: true,
+			smoothHeight: true,
+			slideshowSpeed: 7000,
+			animationSpeed: 600,
+			randomize: false,
+			touch: true,
+		});
 
-   });
+	});
 
 
 	/*----------------------------------------------------*/
-  	/* Smooth Scrolling
-  	------------------------------------------------------*/
-  	$('.smoothscroll').on('click', function (e) {
-	 	
-	 	e.preventDefault();
+	/* Smooth Scrolling
+	------------------------------------------------------*/
+	$('.smoothscroll').on('click', function (e) {
 
-   	var target = this.hash,
-    	$target = $(target);
+		e.preventDefault();
 
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
-      	window.location.hash = target;
-      });
+		var target = this.hash,
+			$target = $(target);
 
-  	});  
-  
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 800, 'swing', function () {
+			window.location.hash = target;
+		});
 
-   /*----------------------------------------------------*/
+	});
+
+
+	/*----------------------------------------------------*/
 	/*  Placeholder Plugin Settings
-	------------------------------------------------------*/ 
+	------------------------------------------------------*/
 
-	$('input, textarea, select').placeholder()  
+	$('input, textarea, select').placeholder()
 
 
 	/*---------------------------------------------------- */
-   /* ajaxchimp
-	------------------------------------------------------ */
+	/* ajaxchimp
+	 ------------------------------------------------------ */
 
 	// Example MailChimp url: http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
 	var mailChimpURL = 'http://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e65110b38d'
@@ -157,7 +139,7 @@
 	$('#mc-form').ajaxChimp({
 
 		language: 'es',
-	   url: mailChimpURL
+		url: mailChimpURL
 
 	});
 
@@ -173,54 +155,54 @@
 	//  5: 'This email address looks fake or invalid. Please enter a real email address'
 
 	$.ajaxChimp.translations.es = {
-	  'submit': 'Submitting...',
-	  0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
-	  1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
-	  2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-	  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-	  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-	  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
+		'submit': 'Submitting...',
+		0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
+		1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
+		2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+		3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+		4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+		5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
 	}
 
 
 	/*---------------------------------------------------- */
 	/* FitVids
-	------------------------------------------------------ */ 
-  	$(".fluid-video-wrapper").fitVids();
+	------------------------------------------------------ */
+	$(".fluid-video-wrapper").fitVids();
 
 
- 	/*---------------------------------------------------- */
+	/*---------------------------------------------------- */
 	/*	Modal Popup
 	------------------------------------------------------ */
 
-    $('.video-link a').magnificPopup({
+	$('.video-link a').magnificPopup({
 
-       type:'inline',
-       fixedContentPos: false,
-       removalDelay: 200,
-       showCloseBtn: false,
-       mainClass: 'mfp-fade'       
+		type: 'inline',
+		fixedContentPos: false,
+		removalDelay: 200,
+		showCloseBtn: false,
+		mainClass: 'mfp-fade'
 
-    });
+	});
 
-    $(document).on('click', '.close-popup', function (e) {
-    		e.preventDefault();
-    		$.magnificPopup.close();
-    });
+	$(document).on('click', '.close-popup', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
 
 
- 	/*----------------------------------------------------- */
-  	/* Back to top
-   ------------------------------------------------------- */ 
+	/*----------------------------------------------------- */
+	/* Back to top
+------------------------------------------------------- */
 	var pxShow = 300; // height on which the button will show
 	var fadeInTime = 400; // how slow/fast you want the button to show
 	var fadeOutTime = 400; // how slow/fast you want the button to hide
 	var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
 
-   // Show or hide the sticky footer button
-	jQuery(window).scroll(function() {
+	// Show or hide the sticky footer button
+	jQuery(window).scroll(function () {
 
-		if (!( $("#header-search").hasClass('is-visible'))) {
+		if (!($("#header-search").hasClass('is-visible'))) {
 
 			if (jQuery(window).scrollTop() >= pxShow) {
 				jQuery("#go-top").fadeIn(fadeInTime);
@@ -228,9 +210,9 @@
 				jQuery("#go-top").fadeOut(fadeOutTime);
 			}
 
-		}		
+		}
 
-	});		
+	});
 
 })(jQuery);
 
